31ec573de96ea2ebc20264a2ace5dc54027de700
narayanareddy <narayanareddychinnapureddy@yahoo.com>
2021-10-25 17:58:17 +0530

    worked on curying

--- 10_JS_DOM_Manipulation/10_script.js
+++ 10_JS_DOM_Manipulation/10_script.js
@@ -14,16 +14,16 @@ console.log(document.body);
 let navtag = document.querySelector('nav');
 console.log(navtag);
 
-//anchor tag 
+//anchor tag
 let anchortag = document.querySelector('nav a');
 console.log(anchortag);
 
-//Inner text in anchor tag 
+//Inner text in anchor tag
 let text = anchortag.innerText;
 console.log(text);
 
 //changing the inner text
-//anchortag.innerText = 'Narayana Reddy' 
+//anchortag.innerText = 'Narayana Reddy'
 
 //accessing the h1 tag
 
@@ -31,7 +31,7 @@ let h1Tag = document.querySelector('#msg');
 console.log(h1Tag);
 
 //change text
-h1Tag.innerText = 'Hi Everybody!!'
+h1Tag.innerText = 'Hi Everybody!!';
 
 //styles
 
@@ -42,31 +42,29 @@ h1Tag.style.padding = '25px';
 h1Tag.style.textAlign = 'center';
 h1Tag.style.boxShadow = '0 0 10px black';
 
-
 /* Telsuko JavaScript */
 
 //getElementById
 function buttonClick() {
-  document.getElementById('heading').innerHTML = "You have changed the text"
+	document.getElementById('heading').innerHTML = 'You have changed the text';
 }
 
 //taking input form user
 function fn1() {
-  var username = document.getElementById('text-1').value;
-  var password = document.getElementById('password-1').value;
-  if(username == password ){
-    alert("username and password are same")
-  }
-  else {
-    alert("username and password are not same")
-  }
+	var username = document.getElementById('text-1').value;
+	var password = document.getElementById('password-1').value;
+	if (username == password) {
+		alert('username and password are same');
+	} else {
+		alert('username and password are not same');
+	}
 }
 
 //getElementByTagName
 function changeStyle() {
-  var para = document.getElementsByTagName("p");
-  para[0].style.fontSize = 50;
-  para[1].style.color = "red";
-  para[2].style.fontWeight = "bold";
-  para[3].style.fontStyle = "italic";
+	var para = document.getElementsByTagName('p');
+	para[0].style.fontSize = 50;
+	para[1].style.color = 'red';
+	para[2].style.fontWeight = 'bold';
+	para[3].style.fontStyle = 'italic';
 }
--- 52_JS_Function_Currying/52_Function_Currying.js
+++ 52_JS_Function_Currying/52_Function_Currying.js
@@ -1,34 +1,67 @@
 //function currying using bind method
 
-let multiply = function (x, y){
-  console.log(x * y);
+let multiply = function (x, y) {
+	console.log(x * y);
 };
 
 //this is similar to below function
 
-/* let multiplyByTwo = function (y){
-  let x = 2; //setting x=2 permanently
-  console.log(x * y);
-} */
+/* let multiplyByTwo = function (y) {
+	let x = 2; //setting x=2 permanently
+	console.log(x * y);
+}; */
 
 let multiplyByTwo = multiply.bind(this, 2);
 //this key word sets x=2 permanently like above function
 multiplyByTwo(5); //y=5 and output is 10
 
-let multiplyByFive = multiply.bind(this,5);
+//Note: if suppose 2, 12 pass chesthe, answer 24 vasthunddi. 5 ni ignore chesthunddi
+
+let multiplyByFive = multiply.bind(this, 5);
 //here this key word sets x=5
 multiplyByFive(4); //y=4 and output is 20
 
-
 //function currying using closures
-let doMultiply = function(x){
-  return function(y){
-    console.log(x * y);
-  }
-}
+let doMultiply = function (x) {
+	return function (y) {
+		console.log(x * y);
+	};
+};
 
 let doMultiplyByTwo = doMultiply(2);
 doMultiplyByTwo(4);
 
 let doMultiplyByThree = doMultiply(3);
 doMultiplyByThree(4);
+
+// realtime example for currying, closure, partial application
+let jsonPlaceholder = async (endpoint) => {
+	let url = `http://jsonplaceholder.typicode.com/${endpoint}`;
+	let resp = await fetch(url);
+	let data = await resp.json();
+	return (num) => {
+		return data
+			.slice(0, num)
+			.map((item) => {
+				let label = item.name || item.title;
+				return `<p>${endpoint} :: ${label}</p>`;
+			})
+			.join('\n');
+	};
+};
+
+const sleep = (milliseconds) => {
+	return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+async function init() {
+	let posts = await jsonPlaceholder('posts');
+	let users = await jsonPlaceholder('users');
+
+	sleep(3000).then(() => {
+		//after 3 seconds this runs
+		console.log(posts(2));
+		console.log(users(4));
+	});
+}
+init();
